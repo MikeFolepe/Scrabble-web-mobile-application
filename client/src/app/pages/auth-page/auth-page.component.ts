6cfabd18ef345b8c0e2cf03ae6aa364267ffb80a
Amir Cherkaoui <amircherk@Amirs-MacBook-Pro.local>
2022-09-22 00:17:45 -0400

    Amelioration du systeme d'authentification et mise en place de l'authentification pour le prototype

--- client/src/app/pages/auth-page/auth-page.component.ts
+++ client/src/app/pages/auth-page/auth-page.component.ts
@@ -14,12 +14,21 @@ export class AuthPageComponent implements OnInit {
   constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder ) { }
   authForm: FormGroup;
   isSubmitted  =  false;
+  validIpAddress = true;
+  validPseudonym = true;
   hasAccount = true;
+  notEmpty = true;
+  pseudonymValue = "";
+  ipAddressValue = "";
+
 
   ngOnInit() {
     this.authForm  =  this.formBuilder.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required]
+      pseudonym: ['', Validators.required],
+      ipAddress: ['', Validators.required],
+      
+      // email: ['', Validators.required],
+      // password: ['', Validators.required]
     });
   }
 
@@ -27,11 +36,27 @@ export class AuthPageComponent implements OnInit {
 
   signIn(){
     this.isSubmitted = true;
+
     if(this.authForm.invalid){
+
       return;
     }
+
+    if (this.pseudonymValue != "amir") {
+      this.validPseudonym = false;
+      this.notEmpty = true;
+      return;
+    }
+    else if (this.ipAddressValue != "12345") {
+      this.validIpAddress = false;
+      this.notEmpty = true;
+      return;
+    }
+    
+
     this.authService.signIn(this.authForm.value);
     this.router.navigateByUrl('/home');
+    
   }
 
   test() {
@@ -43,7 +68,12 @@ export class AuthPageComponent implements OnInit {
     this.hasAccount = false;
   }
 
+  onKeyPseudonym(event: any) { 
+    this.pseudonymValue = event.target.value;
+  }
 
-
+  onKeyIpAddress(event: any) {
+    this.ipAddressValue = event.target.value;
+  }
 
 }
--- client/src/user.ts
+++ client/src/user.ts
@@ -1,4 +1,5 @@
 export interface User {
-    email: string;
+    ipAddress: string;
+    pseudonym: string;
     password: string;
 }
