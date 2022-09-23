fb9ce5f12a97a305fe06f6a98e538c4c82c3005e
mike-davy.folepe-diffo@polymtl.ca <78043104+MikeFolepe@users.noreply.github.com>
2022-09-22 20:45:01 -0400

    Ajout du chat

---
+++ server-nest/app/gateways/chat/chatbox.gateway.ts
@@ -0,0 +1,45 @@
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+// import { RoomManagerService } from '../services/room-manager/room-manager.service';
+import { Server } from 'socket.io';
+
+@WebSocketGateway({
+    cors: {
+        origin: '*',
+    },
+})
+export class ChatboxGateway {
+    // roomManagerService: RoomManagerService;
+
+    @WebSocketServer()
+    server: Server;
+    nbCLients: number;
+
+    // handleConnection(socket: io.Socket) {
+    //   socket.on('createRoom', (gameSettings: GameSettings, gameType: GameType) => {
+    //     const roomId = this.roomManagerService.createRoomId(gameSettings.playersNames[PlayerIndex.OWNER], socket.id);
+    //     this.roomManagerService.createRoom(socket.id, roomId, gameSettings, gameType);
+    //     socket.join(roomId);
+    //     // give the client his roomId to communicate later with server
+    //     socket.emit('yourRoomId', roomId);
+    //     // room creation alerts all clients on the new rooms configurations
+    //     this.sio.emit('roomConfiguration', this.roomManagerService.rooms);
+    //     // Send number of rooms available
+    //     this.sio.emit('roomAvailable', this.roomManagerService.getNumberOfRoomInWaitingState());
+    // });
+    // }
+
+    @SubscribeMessage('sendRoomMessage')
+    handleMessage(@MessageBody() message: string): void {
+        this.server.emit('receiveRoomMessage', message);
+    }
+
+    // @SubscribeMessage("getRoomsConfiguration")
+    // roomConfiguration(client: Socket){
+    //   client.emit('roomConfiguration', this.roomManagerService.rooms);
+    // }
+
+    // @SubscribeMessage("connection")
+    // roomConfiguration(client: Socket){
+    //   console.log("sdfds");
+    // }
+}
