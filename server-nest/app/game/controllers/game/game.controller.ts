87b1f1974c3528e485746a68968e0d14465f5f96
mifola <mike-davy.folepe-diffo@polymtl.ca>
2022-10-16 15:21:29 -0400

    Validation moved to nest server

--- server-nest/app/game/controllers/game/game.controller.ts
+++ server-nest/app/game/controllers/game/game.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Get, HttpStatus, Req, Res } from '@nestjs/common';
+import { WordValidationService } from '@app/game/services/word-validation.service';
+import { Controller, Get, HttpStatus, Post, Req, Res } from '@nestjs/common';
+import { Response } from 'express';
 import * as fileSystem from 'fs';
 @Controller('game')
 export class GameController {
+    constructor(private wordValidator: WordValidationService) {}
+
     @Get('/dictionary/:fileName')
-    getDictionnary(@Req() request, @Res() response) {
+    getDictionnary(@Req() request, @Res() response: Response) {
         const readFile = JSON.parse(fileSystem.readFileSync(`./dictionaries/${request.params.fileName}`, 'utf8'));
         const words = readFile.words;
         response.status(HttpStatus.OK).send(words);
+    }
+
+    @Post('/validateWords/:fileName')
+    validateWords(@Req() request, @Res() response: Response) {
+        const isValid = this.wordValidator.isValidInDictionary(request.body, request.params.fileName);
+        response.status(HttpStatus.OK).send(isValid);
     }
 }
