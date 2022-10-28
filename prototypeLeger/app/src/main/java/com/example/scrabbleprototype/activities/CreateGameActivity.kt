0c6ab2adab34e9084ad82b3fc053b413b6852b55
dieyba <105307287+didiallo@users.noreply.github.com>
2022-10-28 07:57:23 -0400

    dd sending game parameter to server to create the room

--- prototypeLeger/app/src/main/java/com/example/scrabbleprototype/activities/CreateGameActivity.kt
+++ prototypeLeger/app/src/main/java/com/example/scrabbleprototype/activities/CreateGameActivity.kt
@@ -1,15 +1,17 @@
 package com.example.scrabbleprototype.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import com.example.scrabbleprototype.R
-import com.example.scrabbleprototype.model.Dictionary
-import com.example.scrabbleprototype.model.Users
+import com.example.scrabbleprototype.model.*
+import com.example.scrabbleprototype.objects.CurrentRoom
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
@@ -24,7 +26,11 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import org.json.JSONArray
+import org.json.JSONObject
+import java.util.ArrayList
 import kotlin.coroutines.CoroutineContext
 
 class CreateGameActivity : AppCompatActivity(), CoroutineScope {
@@ -36,12 +42,13 @@ class CreateGameActivity : AppCompatActivity(), CoroutineScope {
         super.onDestroy()
         job.cancel()
     }
-
-    val minutes = arrayListOf<String>("00", "01", "02", "03")
-    val seconds = arrayListOf<String>("00", "30")
+    var currentRoom = CurrentRoom;
+    var gameSetting: GameSettings = GameSettings(arrayListOf(Users.currentUser), StartingPlayer.Player1, "00", "00", AiType.beginner, "", "", "", arrayOf())
+    val minutes = arrayListOf("00", "01", "02", "03")
+    val seconds = arrayListOf("00", "30")
     var dictionariesTitle =  arrayListOf<String>()
-    var timerSet = arrayListOf<String>("00", "00")
     lateinit var client: HttpClient
+    val playerSocket = SocketHandler.getPlayerSocket()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,9 +59,8 @@ class CreateGameActivity : AppCompatActivity(), CoroutineScope {
                 json()
             }
         }
-        Log.d("ipadress", Users.ipAddress)
-
         setupSpinners()
+        setUpButtons()
     }
 
     fun setupSpinners() {
@@ -68,38 +74,34 @@ class CreateGameActivity : AppCompatActivity(), CoroutineScope {
         launch {
             val response = getDictionaries()
             if(response != null) {
-                Log.d("if response", "yes")
                 val stringBody: String = response.body();
                 val mapper = jacksonObjectMapper()
-                Log.d("dico", stringBody)
                 val dictionary: List<Dictionary> = mapper.readValue(stringBody, object: TypeReference<List<Dictionary>>() {})
-                Log.d("dico", dictionary.toString())
                 for (i in 0 until dictionary.size) {
                        dictionariesTitle.add(dictionary[i].title)
                 }
             }
             Log.d("dico", dictionariesTitle.toString())
         }
-
-        dicoSpinner.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, dictionariesTitle)
+        dicoSpinner!!.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, dictionariesTitle)
 
         handleMinutesSelection(minutesSpinner)
         handleSecondesSelection(secondsSpinner)
+        handleDictionarySelection(dicoSpinner)
 
         minutesSpinner.setSelection(1)
+        dicoSpinner.setSelection((0))
     }
 
     suspend fun getDictionaries(): HttpResponse? {
         var response: HttpResponse?
         try {
-            Log.d("try", "yes")
             response = client.get(Users.ipAddress + "/api/admin/dictionaries") {
                 contentType(ContentType.Application.Json)
             }
         } catch (err: Exception) {
            response = null
         }
-        Log.d("response", response.toString())
         return response
     }
 
@@ -111,7 +113,7 @@ class CreateGameActivity : AppCompatActivity(), CoroutineScope {
                 position: Int,
                 id: Long
             ) {
-                timerSet[0]=minutes[position]
+                gameSetting.timeMinute=minutes[position]
                 Log.d("spinner", minutes[position])
             }
 
@@ -128,7 +130,7 @@ class CreateGameActivity : AppCompatActivity(), CoroutineScope {
                 position: Int,
                 id: Long
             ) {
-                timerSet[1] = seconds[position]
+                gameSetting.timeSecond = seconds[position]
                 Log.d("spinner", seconds[position])
             }
 
@@ -136,5 +138,43 @@ class CreateGameActivity : AppCompatActivity(), CoroutineScope {
             }
 
         }
+    }
+
+    private fun handleDictionarySelection(dicoSpinner: Spinner) {
+        dicoSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                gameSetting.dictionary = dictionariesTitle[position]
+                Log.d("dico", dictionariesTitle[position])
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+        }
+    }
+
+    private fun setUpButtons() {
+        val continueGameButton = findViewById<Button>(R.id.continue_button)
+        continueGameButton.setOnClickListener {
+            createGame(this.gameSetting)
+        }
+        val backButton = findViewById<Button>(R.id.back_button)
+        backButton.setOnClickListener {
+            startActivity(Intent(this, HomeMenuActivity::class.java))
+        }
+    }
+
+    private fun createGame (gameSetting: GameSettings) {
+        val gameType = 0;
+        playerSocket.on("yourRoomId") { response ->
+            var roomReceived = Room(response[0].toString(), arrayListOf(playerSocket.id()), gameSetting, State.Waiting)
+            currentRoom.myRoom = roomReceived;
+        }
+        playerSocket.emit("createRoom", JSONObject(Json.encodeToString(gameSetting)), Json.encodeToString(gameType))
     }
 }
