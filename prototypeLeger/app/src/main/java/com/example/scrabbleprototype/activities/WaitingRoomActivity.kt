9b7725c87bd44b9a2a7f020b9d348e8e89ae4bd1
dieyba <105307287+didiallo@users.noreply.github.com>
2022-10-30 01:54:25 -0400

    dd adding view for converting a public game to private

--- prototypeLeger/app/src/main/java/com/example/scrabbleprototype/activities/WaitingRoomActivity.kt
+++ prototypeLeger/app/src/main/java/com/example/scrabbleprototype/activities/WaitingRoomActivity.kt
@@ -1,7 +1,9 @@
 package com.example.scrabbleprototype.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.scrabbleprototype.R
 
 class WaitingRoomActivity : AppCompatActivity() {
@@ -10,7 +12,19 @@ class WaitingRoomActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.waiting_room)
+
+        setUpButtons()
+        }
+
+    private fun setUpButtons(){
+        val privateRadioButton = findViewById<Button>(R.id.convert_game)
+        privateRadioButton.setOnClickListener {
+            val intent = Intent(this, PrivateGamePwd::class.java)
+            intent.putExtra("popuptitle", "Veuillez entrer un mot de passe")
+            startActivity(intent)
+
+        }
+    }
     }
 
 
-}
