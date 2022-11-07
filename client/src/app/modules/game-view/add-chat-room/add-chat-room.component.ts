506b3587838f578b1ae4fbe19cf13e1cb2ed12f6
Amir Cherkaoui <amircherk@Sansfil-Eduroam-Etudiants-13-127.polymtl.ca>
2022-11-07 11:46:30 -0500

    Polishing de code

--- client/src/app/modules/game-view/add-chat-room/add-chat-room.component.ts
+++ client/src/app/modules/game-view/add-chat-room/add-chat-room.component.ts
@@ -13,6 +13,7 @@ export class AddChatRoomComponent implements OnInit {
 
   chatRoomName: string;
   errorMessage: string;
+  secondErrorMessage : string;
 
   constructor(public addChatRoomDialogRef: MatDialogRef<AddChatRoomComponent>, private clientSocketService : ClientSocketService, private authService : AuthService, private chatRoomService : ChatRoomService) { 
   }
@@ -24,16 +25,18 @@ export class AddChatRoomComponent implements OnInit {
     this.clientSocketService.socket.emit('createChatRoom', this.authService.currentUser, this.chatRoomName);
   }
 
-  //function that checks if the chatroom name is a minimum of 8 characters, a maximum of 20 characters, and contains only letters and numbers
+  
   validChatRoomName() {
-    var regex = /^[a-zA-Z0-9]{8,20}$/;
+
+    const regex = /^[a-zA-Z0-9]+[a-zA-Z0-9 ]{3,20}[a-zA-Z0-9]$/;
     
     if (this.chatRoomName == null) {
       this.errorMessage = "Le nom du canal ne peut être vide.";
       return false;
     }
     else if (!regex.test(this.chatRoomName)) {
-      this.errorMessage = "Le nom du canal doit contenir entre 8 et 20 caractères et ne peut contenir que des lettres et des chiffres.";
+      this.errorMessage = "Le nom du canal doit contenir au moins 5 caractères, les caractères étant seulement des lettres et des chiffres."
+      this.secondErrorMessage = "Les espaces doivent être suivis de caractères.";
       return false;
     }
     else if (Boolean(this.chatRoomService.chatRooms.find((chatRoom) => chatRoom.chatRoomName === this.chatRoomName))){
