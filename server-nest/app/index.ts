be152eda468fff3956ae4c82ac4df402782dc0ce
Aziz Diop <mouhamed-abdoul-aziz.diop@polymtl.ca>
2022-11-13 08:31:11 -0500

    delpoyment

--- server-nest/app/index.ts
+++ server-nest/app/index.ts
@@ -1,7 +1,10 @@
 import { AppModule } from '@app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
+import * as functions from 'firebase-functions';
 
 const bootstrap = async () => {
     const app = await NestFactory.create(AppModule);
@@ -20,5 +23,14 @@ const bootstrap = async () => {
 
     await app.listen(process.env.PORT);
 };
+const expressServer = express();
+const createFunction = async (expressInstance): Promise<void> => {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressInstance));
+    await app.init();
+};
+export const api = functions.https.onRequest(async (request, response) => {
+    await createFunction(expressServer);
+    expressServer(request, response);
+});
 
 bootstrap();
