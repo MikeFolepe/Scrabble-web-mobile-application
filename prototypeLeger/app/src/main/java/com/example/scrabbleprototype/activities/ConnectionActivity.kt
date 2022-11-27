b58fbe24f24abda26c4448bd20b53222a5401b24
Étienne Chevalier Tittley <tienne.chevalier-tittley@polymtl.ca>
2022-11-27 17:11:19 +0000

    Bug fix develop

--- prototypeLeger/app/src/main/java/com/example/scrabbleprototype/activities/ConnectionActivity.kt
+++ prototypeLeger/app/src/main/java/com/example/scrabbleprototype/activities/ConnectionActivity.kt
@@ -44,7 +44,7 @@ class ConnectionActivity : AppCompatActivity(), CoroutineScope {
 
     private var job: Job = Job()
     override val coroutineContext: CoroutineContext
-        get() = Dispatchers.Default + job
+        get() = Dispatchers.Main + job
 
     override fun onDestroy() {
         super.onDestroy()
