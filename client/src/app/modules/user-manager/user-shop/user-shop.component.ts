a9097417f6af91b557a858bae77443acd4273637
Amir Cherkaoui <amircherk@Sansfil-Eduroam-Etudiants-36-234.polymtl.ca>
2022-12-02 23:20:57 -0500

    Ajout des chatrooms dans les options

--- client/src/app/modules/user-manager/user-shop/user-shop.component.ts
+++ client/src/app/modules/user-manager/user-shop/user-shop.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddChatRoomComponent } from '@app/modules/game-view/add-chat-room/add-chat-room.component';
+import { ChangeChatRoomComponent } from '@app/modules/game-view/change-chat-room/change-chat-room.component';
+import { JoinChatRoomsComponent } from '@app/modules/game-view/join-chat-rooms/join-chat-rooms.component';
 import { galaxy, gradient, tartan } from '@common/themePics';
 
 @Component({
@@ -10,11 +14,23 @@ export class UserShopComponent implements OnInit {
     galaxy: string;
     tartan: string;
     gradient: string;
-    constructor() {
+    constructor(public joinChatRoomsDialog: MatDialog, public changeChatRoomDialog: MatDialog,public addChatRoomDialog: MatDialog) {
         this.gradient = gradient;
         this.galaxy = galaxy;
         this.tartan = tartan;
     }
 
     ngOnInit(): void {}
+
+    openChangeChatRoomDialog(): void {
+        this.changeChatRoomDialog.open(ChangeChatRoomComponent, { disableClose: true });
+    }
+
+    openJoinChatRoomDialog(): void {
+        this.joinChatRoomsDialog.open(JoinChatRoomsComponent, { disableClose: true });
+    }
+
+    openAddChatRoomDialog(): void {
+        this.addChatRoomDialog.open(AddChatRoomComponent, { disableClose: true });
+    }
 }
--- client/src/app/modules/user-manager/user-stats/user-stats.component.ts
+++ client/src/app/modules/user-manager/user-stats/user-stats.component.ts
@@ -1,5 +1,9 @@
 /* eslint-disable no-underscore-dangle */
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddChatRoomComponent } from '@app/modules/game-view/add-chat-room/add-chat-room.component';
+import { ChangeChatRoomComponent } from '@app/modules/game-view/change-chat-room/change-chat-room.component';
+import { JoinChatRoomsComponent } from '@app/modules/game-view/join-chat-rooms/join-chat-rooms.component';
 import { AuthService } from '@app/services/auth.service';
 import { UserService } from '@app/services/user.service';
 
@@ -9,9 +13,21 @@ import { UserService } from '@app/services/user.service';
     styleUrls: ['./user-stats.component.scss'],
 })
 export class UserStatsComponent implements OnInit {
-    constructor(public userService: UserService, public authService: AuthService) {}
+    constructor(public userService: UserService, public authService: AuthService, public joinChatRoomsDialog: MatDialog, public changeChatRoomDialog: MatDialog,public addChatRoomDialog: MatDialog) {}
 
     ngOnInit(): void {
         this.userService.getUserStats(this.authService.currentUser._id);
+    }
+
+    openChangeChatRoomDialog(): void {
+        this.changeChatRoomDialog.open(ChangeChatRoomComponent, { disableClose: true });
+    }
+
+    openJoinChatRoomDialog(): void {
+        this.joinChatRoomsDialog.open(JoinChatRoomsComponent, { disableClose: true });
+    }
+
+    openAddChatRoomDialog(): void {
+        this.addChatRoomDialog.open(AddChatRoomComponent, { disableClose: true });
     }
 }
